591768015e145b03166de4979159bd18faeecec6
Viktar Kolbik <Viktar_Kolbik@epam.com>
2017-12-13 14:22:13 +0300

    init commit

---
+++ src/main/java/com/howtocook/howtocook/HowtocookApplication.java
@@ -0,0 +1,12 @@
+package com.app.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtocookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowtocookApplication.class, args);
+	}
+}
